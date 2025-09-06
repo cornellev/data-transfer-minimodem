ee4ef1c0599ddbfa7f898570b1e59b992ca45b51
kt <katie.xiao@outlook.com>
2025-09-06 17:49:41 -0400

    threading

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,37 +1,41 @@
 import subprocess
 import schema.example_pb2 as example_pb2
 
-def main():
+baud = 200
+START = b"\x02"   
+END   = b"\x03"  
 
-    baud = 200
+def main():
     proc = subprocess.Popen(
         ["minimodem", "--rx", "-8", str(baud)],
         stdout=subprocess.PIPE
     )
-
-    START = b"\x02"   
-    END   = b"\x03"   
     raw= b""
 
-    while True:
-        byte = proc.stdout.read(1)
-        if not byte:
-            break
-            
-        raw += byte 
+    try: 
+        while True:
+            byte = proc.stdout.read(1)
+            if not byte:
+                break
+                
+            raw += byte 
 
-        if START in raw and END in raw:
-            start = raw.find(START) + 1
-            end = raw.find(END, start)
-            raw_bytes = raw[start:end]
+            while START in raw and END in raw:
+                start = raw.find(START) + 1
+                end = raw.find(END, start)
+                packet = raw[start:end]
 
-            msg = example_pb2.Example()
-            msg.ParseFromString(raw_bytes)
+                try: 
+                    msg = example_pb2.Example()
+                    msg.ParseFromString(packet)
+                    print(msg)
+                except Exception as e:
+                    print("Failed to parse packet:", e)
+                
+                raw = raw[end+1:]
 
-            print(msg)
-            break
-    
-    proc.terminate()
+    finally:
+        proc.terminate()
 
 
 if __name__ == "__main__":
--- sender/sender.py
+++ sender/sender.py
@@ -1,19 +1,54 @@
-import subprocess 
-import schema.example_pb2 as example_pb2
+import subprocess, queue, time, threading 
+import schema.example_pb2 as example_pb2 
+
+baud = 200
+START = b"\x02"   
+END   = b"\x03"  
+
+packets = queue.Queue()
+
+def producer():
+    """
+    Serialize and push (dummy) packets of sensor data to the queue
+    """
+
+    # Hardcoded two packets
+    for i in range(1, 3):
+        msg = example_pb2.Example(
+            field1=(i),
+            field2=float(i),
+            field3=str(i)
+        )
+
+        data = msg.SerializeToString()
+        packets.put(data)
+
+        time.sleep(1.0)
+
+def consumer():
+    """
+    Transmit the packets of sensor data to the receiver 
+    """
+
+    for _ in range(2):
+        cur = packets.get()
+        data = START + cur + END 
+
+        subprocess.run(
+            ["minimodem", "--tx", str(baud), "-f", "test.wav"], 
+            input=data, 
+            text=False
+        )
 
 def main():
+    prod_thread = threading.Thread(target=producer, daemon=True)
+    cons_thread = threading.Thread(target=consumer, daemon=True)
 
-    msg = example_pb2.Example(field1=1, field2=1.0, field3="Test")
-    data = msg.SerializeToString()
+    prod_thread.start()
+    cons_thread.start()
 
-    baud = 200
-
-    START = b"\x02"   
-    END   = b"\x03"   
-
-    new_data = START + data + END 
-
-    subprocess.run(["minimodem", "--tx", str(baud)], input=new_data, text=False)
+    prod_thread.join()
+    cons_thread.join()
 
 if __name__ == "__main__":
     main()

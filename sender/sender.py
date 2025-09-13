cc37ba4e5c47fdefd4ed49cf7db8fc97deba6793
kt <katie.xiao@outlook.com>
2025-09-13 11:34:48 -0400

    change receiver to piping data

--- sender/sender.py
+++ sender/sender.py
@@ -6,30 +6,37 @@ START = b"\x02"
 END   = b"\x03"  
 
 def main():
+    proc = subprocess.Popen(
+        ["minimodem", "--tx", str(baud)],
+        stdin=subprocess.PIPE,
+        text=False
+    )
     count = 0
-    while True:
 
-        # Dummy data 
-        msg = data_pb2.Sensors(
-            voltage=1.0 + count,
-            draw=2.0 + count, 
-            gps_lat=3.0 + count,
-            gps_long=4.0 + count,
-            velocity=5.0+count,
-            throttle=6.0+count
-        )
+    try:
+        while count < 2:
 
-        data = START + msg.SerializeToString() + END 
+            # Dummy data 
+            msg = data_pb2.Sensors(
+                voltage=1.0 + count,
+                draw=2.0 + count, 
+                gps_lat=3.0 + count,
+                gps_long=4.0 + count,
+                velocity=5.0+count,
+                throttle=6.0+count
+            )
 
-        subprocess.run(
-            ["minimodem", "--tx", str(baud)], 
-            input=data, 
-            text=False
-        )
+            data = START + msg.SerializeToString() + END 
+            proc.stdin.write(data)
+            proc.stdin.flush()
 
-        print(f"Sent packet #{count}")
-        count += 1
-        time.sleep(1.0)
+            print(f"Sent packet #{count + 1}")
+            count += 1
+            time.sleep(1.0)
+    finally:
+        if proc.stdin:
+            proc.stdin.close()
+        proc.wait()
 
 if __name__ == "__main__":
     main()

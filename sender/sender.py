3368cbdf1c4abefce3967945456dbd65c0641c45
Adi Murgescu <murgescuadi@gmail.com>
2025-11-11 18:38:32 -0500

    add command line parsing to switch modes

--- sender/sender.py
+++ sender/sender.py
@@ -1,61 +1,41 @@
 import subprocess, time
+import argparse
 import schema.data_pb2 as data_pb2 
 from config import BAUD, START, END, RECEIVER_NUMBER
 from cellular_modem import CellularModem
 
-def send_packets(n):
-    """Send n packets of protobuf data via minimodem."""
-    proc = subprocess.Popen(
-        ["minimodem", "--tx", str(BAUD)],
-        stdin=subprocess.PIPE,
-        text=False
-    )
-    count = 0
+from modes.udp_mode import UDPMode
+from modes.modem_mode import ModemMode
 
-    try:
-        while count < n:
+def get_mode(args):
+    if args.mode == 'udp':
+        return UDPMode(ip="0.0.0.0", port=5005)
+    elif args.mode == 'modem':
+        return ModemMode(baud=BAUD)
 
-            # Dummy data 
-            msg = data_pb2.Sensors(
-                voltage=1.0 + count,
-                draw=2.0 + count, 
-                gps_lat=3.0 + count,
-                gps_long=4.0 + count,
-                velocity=5.0+count,
-                throttle=6.0+count
-            )
-
-            data = START + msg.SerializeToString() + END 
-            print(str(len(data)))
-            seconds_in_message = (len(data) * 8) / BAUD
-            t_time = 1 - seconds_in_message
-
-            if t_time < 0:
-                raise ValueError("Message is too long for the given baud rate")
-
-            proc.stdin.write(data)
-            proc.stdin.flush()
-
-            count += 1
-            time.sleep(t_time)
-    finally:
-        if proc.stdin:
-            proc.stdin.close()
-        proc.wait()
 
 def main():
-    modem = CellularModem(power_key=6, port="/dev/ttyS0", baud=115200)
+    parser = argparse.ArgumentParser(description="Choose UDP or Modem")
+    parser.add_argument('--mode', choices=['udp', 'modem'], required=True, help="Transmission mode: 'udp' or 'modem'")
 
-    try:
-        modem.power_on()
+    args = parser.parse_args()
+    mode = get_mode(args)
+    if args.mode == 'modem':
+        modem = CellularModem(power_key=6, port="/dev/ttyS0", baud=115200)
 
-        if modem.dial(RECEIVER_NUMBER):
-            print("Call connected. Sending packets:")
-            send_packets(10)
-            modem.hangup()
-    finally:
-        modem.power_down()
-        modem.close()
+        try:
+            modem.power_on()
+
+            if modem.dial(RECEIVER_NUMBER):
+                print("Call connected. Sending packets:")
+                send_packets(10)
+                modem.hangup()
+        finally:
+            modem.power_down()
+            modem.close()
+
+    #if args.mode == 'udp':
+    
 
 if __name__ == "__main__":
     main()
